8dbb661e6a6be58c691d58d28e78fd6de630b3ad
Sia Karamalegos <siakaramalegos@gmail.com>
2023-08-04 12:27:00 -0400

    Attempt to allow iframes

--- background.js
+++ background.js
@@ -1,4 +1,50 @@
+async function configureNetRequest(tabId, domain) {
+  console.log({ domain });
+  const domains = [domain];
+  const headers = [
+    "X-Frame-Options",
+    "Frame-Options",
+    "Content-Security-Policy",
+  ];
+
+  await chrome.declarativeNetRequest.updateSessionRules({
+    removeRuleIds: [1],
+    addRules: [
+      {
+        id: 1,
+        action: {
+          type: "modifyHeaders",
+          responseHeaders: headers.map((h) => ({
+            header: h,
+            operation: "remove",
+          })),
+        },
+        condition: {
+          requestDomains: domains,
+          resourceTypes: ["sub_frame"],
+          tabIds: [tabId],
+        },
+      },
+    ],
+  });
+
+  await chrome.browsingData.remove(
+    {
+      origins: domains.map((d) => `https://${d}`),
+    },
+    {
+      cacheStorage: true,
+      serviceWorkers: true,
+    }
+  );
+}
+
 chrome.action.onClicked.addListener(async (tab) => {
+  const domain = new URL(tab.url).hostname;
+  await configureNetRequest(tab.id, domain);
+  await chrome.tabs.update(tab.id, { url: tab.url });
+  console.log("reloaded");
+
   // Insert the JS file when the user turns the extension on
   await chrome.scripting.executeScript({
     files: ["RespImageLint.js"],
